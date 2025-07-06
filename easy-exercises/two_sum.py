ea61aa30fefbf31ae839dec09158cf39a1af0545
Yonna <yonnaklaassen@gmail.com>
2025-07-06 18:07:27 +0200

    Update two_sum.py

--- easy-exercises/two_sum.py
+++ easy-exercises/two_sum.py
@@ -7,8 +7,18 @@ class Solution:
             for j in range(len(nums)):
                 if i != j and target - (nums[i] + nums[j]) == 0:
                     return [i, j]
+                
+class OptimizedSolution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_to_index = {}
 
-s = Solution()
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
+
+s = OptimizedSolution()
 case1_nums = [2,7,11,15]
 case1_target = 9
 print(s.twoSum(case1_nums, case1_target)) # [0, 1]
@@ -32,3 +42,5 @@ print(s.twoSum(case5_nums, case5_target)) # [0, 2]
 case6_nums = [-1,-2,-3,-4,-5]
 case6_target = -8
 print(s.twoSum(case6_nums, case6_target)) # [2, 4]
+
+
